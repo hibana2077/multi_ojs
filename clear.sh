ac08461f7b78a76e2e5b59fe7b34a9171faa10d1
hibana2077 <hibana2077@gmail.com>
2023-11-23 11:06:00 +0800

    add remove function

---
+++ clear.sh
@@ -0,0 +1,9 @@
+docker rm -f $(docker ps -aq)
+###
+ # @Author: hibana2077 hibana2077@gmail.com
+ # @Date: 2023-11-23 11:04:50
+ # @LastEditors: hibana2077 hibana2077@gmail.com
+ # @LastEditTime: 2023-11-23 11:05:39
+ # @FilePath: \multi_ojs\clear.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
