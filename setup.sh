91e37ad033d083032a6d76c58f20c0a965c2ba9e
hibana2077 <hibana2077@gmail.com>
2023-11-23 11:08:14 +0800

    Add git, vim, and neofetch installations to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
  # @Author: hibana2077 hibana2077@gmail.com
  # @Date: 2023-11-23 10:45:52
  # @LastEditors: hibana2077 hibana2077@gmail.com
- # @LastEditTime: 2023-11-23 10:49:58
+ # @LastEditTime: 2023-11-23 11:08:08
  # @FilePath: \multi_ojs\setup.sh
  # @Description: This is a shell script for installing docker and docker-compose
 ### 
@@ -13,7 +13,7 @@ if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Installing Docker..."
     # Install Docker
     sudo apt-get update
-    sudo apt-get install -y git
+    sudo apt-get install -y git vim neofetch
     git clone https://gist.github.com/40a965b83c11a4e8e195afb006bc13e3.git /tmp/docker-install
     sudo chmod +x /tmp/docker-install/install.sh
     sudo /tmp/docker-install/install.sh
