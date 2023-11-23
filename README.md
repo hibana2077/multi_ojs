a9c8c6afe44aa42ebb82c44a341d13817a0896ce
hibana2077 <hibana2077@gmail.com>
2023-11-23 11:03:08 +0800

    Add initial setup instructions for the multi-objective optimization problem solver

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# multi_ojs
+<!--
+ * @Author: hibana2077 hibana2077@gmail.com
+ * @Date: 2023-11-23 09:09:32
+ * @LastEditors: hibana2077 hibana2077@gmail.com
+ * @LastEditTime: 2023-11-23 11:03:02
+ * @FilePath: \multi_ojs\README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+# Multi OJS
+
+This repository contains code for a multi-objective optimization problem solver.
+
+## Installation
+
+To install the solver, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/your-username/multi_ojs.git`
+2. Change into the project directory: `cd multi_ojs`
+3. Install the required dependencies: `bash ./setup.sh`
---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+###
+ # @Author: hibana2077 hibana2077@gmail.com
+ # @Date: 2023-11-23 10:45:52
+ # @LastEditors: hibana2077 hibana2077@gmail.com
+ # @LastEditTime: 2023-11-23 10:49:58
+ # @FilePath: \multi_ojs\setup.sh
+ # @Description: This is a shell script for installing docker and docker-compose
+### 
+
+# Check if Docker is available
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing Docker..."
+    # Install Docker
+    sudo apt-get update
+    sudo apt-get install -y git
+    git clone https://gist.github.com/40a965b83c11a4e8e195afb006bc13e3.git /tmp/docker-install
+    sudo chmod +x /tmp/docker-install/install.sh
+    sudo /tmp/docker-install/install.sh
+    echo "Docker installed successfully."
+else
+    echo "Docker is already installed."
+fi
+
+# Check if Docker Compose is available
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose is not installed. Installing Docker Compose..."
+    # Install Docker Compose
+    sudo apt-get update
+    sudo apt-get install -y docker-compose
+    echo "Docker Compose installed successfully."
+else
+    echo "Docker Compose is already installed."
+fi
